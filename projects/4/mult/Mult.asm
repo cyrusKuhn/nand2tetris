90f60166285f4f3815680b6a55a01957570e8792
Cyrus Kuhn <cbk014@bucknell.edu>
2026-09-02 13:58:40 -0400

    Finished Mult assembly for Project 4

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -8,3 +8,63 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+
+(RESTART)
+    @MASK
+    M=1
+    @3
+    M=0
+
+    @0
+    D=M
+    @MODIFIEDA
+    M=D
+
+    @1
+    D=M
+    @MODIFIEDB
+    M=D
+
+    @2
+    M=0
+
+(MULTIPLY)
+    // skip add
+    @MASK
+    D=M
+    @0
+    D=D&M
+    @SKIPADD
+    D;JEQ
+
+    // add the value of the multiplied b
+    @MODIFIEDB
+    D=M
+    @2
+    M=M+D
+
+    @MASK
+    D=M
+    @MODIFIEDA
+    M=M-D
+
+    D=M
+    @END
+    D;JEQ
+    
+(SKIPADD)
+    @MODIFIEDB
+    D=M
+    M=D+M
+    @MASK
+    D=M
+    M=D+M
+
+    // if not zero repeat
+    D=M
+    @MULTIPLY
+    D;JNE
+
+(END)
+    @END
+    0;JMP
